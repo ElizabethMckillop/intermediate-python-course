d26e696f1d2d8c2f7ef317f6177234122387dfc4
ElizabethMckillop <73965729+ElizabethMckillop@users.noreply.github.com>
2020-11-04 21:51:30 +0000

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,7 +1,10 @@
-import random
+
 def main():
-  roll=random.randint(1,6)
-  print(f'You rolled a {roll}')  
+  import random
+  dice_rolls = 2
+  for i in range(0,dice_rolls):
+    roll = random.randint(1,6)
+    print(f'You rolled a {roll}')
   print('You rolled a die')
 
 if __name__== "__main__":
